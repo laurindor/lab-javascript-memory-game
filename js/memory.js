98a9fda8f9913caaeddde5520030aa90683ee8b5
Lau <lmarcos@LT-ACER-ISA.localdomain>
2021-06-09 22:31:39 +0200

    first lab completely solved, I'm about to cry

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,43 @@
 class MemoryGame {
-  constructor(cards) {
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
-
-  checkIfPair(card1, card2) {
-    // ... write your code here
-  }
-
-  checkIfFinished() {
-    // ... write your code here
+  shuffleCards() { 
+    if (this.cards) {
+      let m = this.cards.length - 1
+      while (m) {
+          const i = Math.floor(Math.random()*m)
+          const randomElement = this.cards[i]
+          this.cards[i] = this.cards[m]
+          this.cards[m] = randomElement
+          m--
+      }
   }
 }
 
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true 
+    } else {
+      return false
+    }
+  }
+
+  checkIfFinished(){
+    if(this.cards.length === 24)
+    return false
+   if(this.pairsGuessed === this.cards.length/2)
+      return true
+    else
+      return false
+  }
+}
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
