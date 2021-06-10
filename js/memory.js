8b715614399dbf61707e0c22f9577053041031e3
Lau <lmarcos@LT-ACER-ISA.localdomain>
2021-06-10 12:16:12 +0200

    dom changes

--- js/memory.js
+++ js/memory.js
@@ -19,18 +19,28 @@ class MemoryGame {
   }
 }
 
+/*sort method would be more accurate or this one "Fisher yates method"
+
+shuffleCards() {
+for (let i= this.cards.lenght -1; i >0; i--) ¨{
+  const j = Math.floor(Math.random() * (i + 1));
+  [this.cards[i], array[j]] = [this.cards[j], this.cards[i]];
+  }
+}
+
+*/
 
   checkIfPair(card1, card2) {
-    this.pairsClicked += 1
+    this.pairsClicked += 1 //counter of the number of tries
     if (card1 === card2) {
-      this.pairsGuessed += 1
+      this.pairsGuessed += 1 //counter of the pairs you have guessed
       return true 
     } else {
       return false
     }
   }
 
-  checkIfFinished(){
+ /* checkIfFinished(){
     if(this.cards.length === 24)
     return false
    if(this.pairsGuessed === this.cards.length/2)
@@ -38,6 +48,17 @@ class MemoryGame {
     else
       return false
   }
+}*/
+//THIS ONE IS BETTER
+
+checkIfFinished() {
+  let correctPairs = this.pairsGuessed;
+  if (correctPairs < this.cards.length / 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
 }
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
